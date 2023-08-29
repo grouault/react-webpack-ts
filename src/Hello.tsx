65fe9f7ef77ec7b5754ca892f03e9b1b5b87c1aa
Gildas Rouault <g.rouault@groupe-pomona.fr>
2023-08-29 13:45:51 +0200

    first-commit

---
+++ src/Hello.tsx
@@ -0,0 +1,12 @@
+
+import TestComponent from './components/TestComponent';
+
+export default function Hello() {
+  return (
+    <>
+      <TestComponent />
+      <div>Hello from TypeScript !!!</div>
+    </>
+    
+  )
+}
